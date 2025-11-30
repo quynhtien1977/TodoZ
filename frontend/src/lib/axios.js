1cd504271fbd8d4cfdee5c1b6b9b4cd7b0bc4608
Zair0u <quynhtien123123@gmail.com>
2025-11-30 17:23:24 +0700

    test deploy

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === "production" ? "/api" : "http://localhost:5001/api";
 const api = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: BASE_URL,
 });
 
 export default api;
